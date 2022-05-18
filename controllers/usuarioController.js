60e02a37f789e636e9795fd0419d8a3872dded2b
RafaelPatekoski <patekoskieducacao@gmail.com>
2022-05-18 19:55:07 -0300

    Alterações no Login

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -2,7 +2,9 @@ const Usuario = require('../models/Usuario')
 
 const verificarUsuario = async(req, res, next)=>{
     try{
-      req.usuario = req.headers.usuario
+      req.usuario = await Usuario.findById(cookie)
+      console.log(req.usuario.autorizacao)
+      //Verificar se ta pegando o usuario certo
       if(req.usuario === undefined){req.usuario = null}
       else{}
     }catch(error){
@@ -14,6 +16,10 @@ const verificarUsuario = async(req, res, next)=>{
 
 const cadastro = async(req, res)=>{
     if(req.usuario === null){return res.send('Não autenticado')}
+    //Verificar se o usuario tem autorização para criar outro usuario
+    else if(req.usuario.autorizacao === 'usuario'){
+        return res.send('Não autorizado')
+    }
     else{
     let usuario = new Usuario(req.body)
     try{
@@ -29,13 +35,14 @@ const login = async (req, res)=>{
     let usuario = {}
     usuario.usuario = req.body.usuario
     usuario.senha = req.body.senha
+    console.log(usuario)
     const selectedUser = await Usuario.findOne({usuario: usuario.usuario})
     if(!selectedUser){return res.status(400).send('Usuario! ou senha incorreto')}
     if(selectedUser.senha != usuario.senha){
         return res.status(400).send('Usuario ou senha! incorretos')
     }
-    res.send({token: selectedUser._id})
-    res.redirect('/')
+
+    res.send({token: selectedUser.id});
 }
 
 module.exports = {verificarUsuario, cadastro, login}
